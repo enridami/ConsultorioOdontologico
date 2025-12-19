4151c9d2298fe9a98bab63a5dc5711f4196924ca
Enrique Suica <enri124589@gmail.com>
2025-12-19 10:35:31 -0300

    Refactor domain classes to use Persona inheritance
    Updated Odontologo, Paciente, Responsable, and Secretario to extend the new Persona base class, centralizing common fields and logic. Removed redundant fields from Usuario and simplified its implementation. This change improves code maintainability and reduces duplication across domain entities.

--- src/main/java/logica/Responsable.java
+++ src/main/java/logica/Responsable.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package logica;
 
-/**
- *
- * @author Enrique
- */
-public class Responsable {
+import java.util.Date;
+
+public class Responsable extends Persona{
+    private int id_resposable;
+    private String tipo_responsabilidad;
+
+    public Responsable() {
+    }
+
+    public Responsable(int id_resposable, String tipo_responsabilidad, String dni, String nombre, String apellido, String telefono, String direccion, Date fecha_nac) {
+        super(dni, nombre, apellido, telefono, direccion, fecha_nac);
+        this.id_resposable = id_resposable;
+        this.tipo_responsabilidad = tipo_responsabilidad;
+    }
+
+    public int getId_resposable() {
+        return id_resposable;
+    }
+
+    public void setId_resposable(int id_resposable) {
+        this.id_resposable = id_resposable;
+    }
+
+    public String getTipo_responsabilidad() {
+        return tipo_responsabilidad;
+    }
+
+    public void setTipo_responsabilidad(String tipo_responsabilidad) {
+        this.tipo_responsabilidad = tipo_responsabilidad;
+    }
+    
+    
+    
     
 }
