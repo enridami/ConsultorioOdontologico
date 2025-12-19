520828f0a541ad3798a7e5fe9e4874595c9da8d0
Enrique Suica <enri124589@gmail.com>
2025-12-19 10:51:13 -0300

    Add fields and accessors to Horario, Turno, and Usuario
    Introduced private fields, constructors, and getter/setter methods to the Horario, Turno, and Usuario classes to encapsulate their properties and provide accessors. This enhances data encapsulation and prepares the classes for further use in the application.

--- src/main/java/logica/Usuario.java
+++ src/main/java/logica/Usuario.java
@@ -1,5 +1,52 @@
 package logica;
 
 public class Usuario {
+    private int id_usuario;
+    private String nombre_usuario;
+    private String contrasenia;
+    private String rol;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id_usuario, String nombre_usuario, String contrasenia, String rol) {
+        this.id_usuario = id_usuario;
+        this.nombre_usuario = nombre_usuario;
+        this.contrasenia = contrasenia;
+        this.rol = rol;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNombre_usuario() {
+        return nombre_usuario;
+    }
+
+    public void setNombre_usuario(String nombre_usuario) {
+        this.nombre_usuario = nombre_usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    
     
 }
