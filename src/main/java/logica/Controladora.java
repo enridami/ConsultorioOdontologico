0bd337c9c3f51bcbd39ffde254d606f26c73a728
Enrique Suica <enri124589@gmail.com>
2025-12-21 20:08:38 -0300

    Add user creation method and update persistence controllers
    Introduced the crearUsuario method in Controladora and initialized persistence controller instances in ControladoraPersistencia. Also renamed index.jsp.jsp to index.jsp and updated some UI text to Spanish for consistency.

--- src/main/java/logica/Controladora.java
+++ src/main/java/logica/Controladora.java
@@ -1,6 +1,14 @@
 package logica;
 
+import persistencia.ControladoraPersistencia;
+
 
 public class Controladora {
+    
+    ControladoraPersistencia controlPersis = new ControladoraPersistencia();
+    
+    public void crearUsuario(int id, String nombreUsuario, String contrasenia, String rol){
+        
+    }
     
 }
